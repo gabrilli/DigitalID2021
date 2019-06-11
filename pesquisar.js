9c5100c88fe0240b2d8c558c82215efd8396934a
gabrilli <50384585+gabrilli@users.noreply.github.com>
2019-06-11 16:49:30 -0300

    Create pesquisar.js

---
+++ pesquisar.js
@@ -0,0 +1,18 @@
+<script>
+        var frm2 = document.cliente;
+        function pesquisar.cliente() {
+            event.preventDefault();
+            $("#resultadoPesquisa").html("");
+            contract.ID(frm2.nome.value, frm2.CPF.value, frm2.Cel.value, frm1.identificador.value, function (erro, resultado) {
+                if (erro) {
+                    console.log("pesquisarcliente");
+                    console.error(erro);
+                    alert("Houve um erro ao pesquisar o nome do cliente");
+                    //frmPesquisa.matricula.value = "";
+                    return
+                }
+                $("#resultadoPesquisa").html("O proprietário do imóvel informado é: " + resultado);
+            })
+        }
+
+</script>
