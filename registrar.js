6ce67309a2c4b62c9daf4e35a77c46daa373ebda
gabrilli <50384585+gabrilli@users.noreply.github.com>
2019-06-18 21:05:32 -0300

    Update registrar.js

--- registrar.js
+++ registrar.js
@@ -6,7 +6,7 @@ function registrar () {
 event.preventDefault();
 
 $("#statusTransacao").html("&nbsp;");
-contract.gerarid (frm1.Nome.value, frm1.CPF.value, frm1.Celular.value, frm1.Senha.value, {from: web3.eth.accounts[0], gas: 3000000, value: 0}, function (erro, resultado) {
+contract.gerarid (frm1.Nome.value, frm1.CPF.value, frm1.Celular.value, frm1.Senha.value, frm1.now.value, frm1.blocknumber {from: web3.eth.accounts[0], gas: 3000000, value: 0}, function (erro, resultado) {
     if (erro) {
         console.log("registrar");
         console.error(erro);
