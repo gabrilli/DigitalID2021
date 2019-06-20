f73429e7d69d1c7fa19fbaba6880f7d9070f9f78
gabrilli <50384585+gabrilli@users.noreply.github.com>
2019-06-20 13:06:34 -0300

    Update id.sol

--- id.sol
+++ id.sol
@@ -28,7 +28,6 @@ contract Digitalid {
     }
     
     event NovaID (string Nome, uint256 CPF, uint256 Celular, bytes32 Identificador,  uint256 Timestamp, uint256 Bloco);
-    event VerificarID (string Nome, uint256 CPF, uint256 Celular, bytes32 Identificador);
     event Publicar (string Nome, uint256 CPF, uint256 Celular, bytes32 Identificador, uint256 Timestamp, uint256 Bloco);
    
     
@@ -41,9 +40,7 @@ contract Digitalid {
    
     function gerarid (string memory Nome, uint256 CPF, uint256 Celular, string memory Senha) public payable returns (bytes32) {
        
-    
-       
-       bytes32 Identificador = keccak256(abi.encode(Nome, Celular, CPF, Senha));
+           bytes32 Identificador = keccak256(abi.encode(CPF, Senha));
        
         uint256 Timestamp = now;
         uint256 Bloco = block.number;
@@ -57,14 +54,51 @@ contract Digitalid {
             
         
         emit NovaID (Nome, CPF, Celular, Identificador, Timestamp, Bloco);
-        
-        
-        
         emit Publicar (Nome, CPF, Celular, Identificador, Timestamp, Bloco);
         
      
     }    
-        
     
-       
-   }
+    function procurarporcpf (uint CPF) public view returns (string memory Nome, uint Celular, bytes32 Idetificador, uint Timestamp, uint Bloco) {    
+    
+    uint x;
+    
+    for (x=0; x<ListaClientesA.length; x++) {
+        
+        if (CPF == ListaClientesA[x].CPF) {
+            return (ListaClientesA[x].Nome, ListaClientesA[x].Celular, ListaClientesA[x].Identificador, ListaClientesA[x].Timestamp, ListaClientesA[x].Bloco); 
+        }
+        
+    }
+    
+    }
+    
+    function procurarporhash (bytes32 Identificador) public view returns (string memory Nome, uint Celular, uint CPF, uint Timestamp, uint Bloco) {    
+    
+    uint x;
+    
+    for (x=0; x<ListaClientesA.length; x++) {
+        
+        if (Identificador == ListaClientesA[x].Identificador) {
+            return (ListaClientesA[x].Nome, ListaClientesA[x].Celular, ListaClientesA[x].CPF, ListaClientesA[x].Timestamp, ListaClientesA[x].Bloco); 
+        }
+        
+    }
+    }
+     
+    function procurarporcelular (uint Celular) public view returns (string memory Nome, uint CPF, bytes32 Idetificador, uint Timestamp, uint Bloco) {    
+    
+    uint x;
+    
+    for (x=0; x<ListaClientesA.length; x++) {
+        
+        if (Celular == ListaClientesA[x].Celular) {
+            return (ListaClientesA[x].Nome, ListaClientesA[x].CPF, ListaClientesA[x].Identificador, ListaClientesA[x].Timestamp, ListaClientesA[x].Bloco); 
+        }
+        
+    }
+    } 
+   
+     
+        
+}
